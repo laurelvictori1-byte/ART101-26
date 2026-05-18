9d32d3d5ac2b238e9c38ed3238cc6b0aeb1b7870
laurelvictori1-byte <laurel.victori1@gmail.com>
2026-05-17 22:32:52 -0700

    lem

---
+++ environ/env05.js
@@ -0,0 +1,129 @@
+let environmentTitle = "Classroom";
+
+let environmentElements = ["tables", "chairs", "text box", "len", "doors"];
+
+let mainEntity = {
+    name: "Hatsuni Miki",
+    character: "Childhood Friend",
+    mood: ["happy", "upset"],
+    isFriendInterest: true,
+    favoriteFoods: ["melon pan", "taiyaki", "shaved ice", "leeks"],
+    moodCount: 0,
+    favoriteFoodsCount: 0
+};
+
+let songList = ["blackvow.mp3", "butterfly.mp3", "dreameater.mp3", "halloween.mp3", "remote.mp3"];
+
+function fadeText(newDialogue) {
+    console.log("fade is clicked");
+    $("#text-1").fadeOut(1000, function() {
+        $("#text-1").html("<p>" + newDialogue + "</p>").fadeIn(1000);
+    });
+}
+
+function playAudio (musicType) {
+    let song = new Audio(musicType);
+    song.play();
+    return song
+}
+
+function stopAudio (songToPause) {
+    songToPause.pause();
+}
+
+let curSong = null;
+let songPlaying = false;
+
+function chooseAudio () {
+    let userChoice = prompt("What do you want me to sing? " +
+    "Something dramatic, something upbeat, something jazzy, something seasonal, or something electronic?"
+    );
+
+    if (userChoice.toLowerCase() == "dramatic" || userChoice.toLowerCase() == "something dramatic") {
+        if (!songPlaying){
+            curSong = playAudio ("blackvow.mp3");
+            songPlaying = true;
+        } else {
+            stopAudio (curSong);
+            curSong = playAudio ("blackvow.mp3");
+        }
+    }
+    else if (userChoice.toLocaleLowerCase() == "upbeat" || userChoice.toLowerCase() == "something upbeat") {
+        if (!songPlaying){
+            curSong = playAudio ("butterfly.mp3");
+            songPlaying = true;
+        } else {
+            stopAudio (curSong);
+            curSong = playAudio ("butterfly.mp3");
+        }
+    }
+    else if (userChoice.toLocaleLowerCase() == "jazzy" || userChoice.toLowerCase() == "something jazzy") {
+        if (!songPlaying){
+            curSong = playAudio ("dreameater.mp3");
+            songPlaying = true;
+        } else {
+            stopAudio (curSong);
+            curSong = playAudio ("dreameater.mp3");
+        }
+    }
+    else if (userChoice.toLocaleLowerCase() == "seasonal" || userChoice.toLowerCase() == "something seasonal") {
+        if (!songPlaying){
+            curSong = playAudio ("halloween.mp3");
+            songPlaying = true;
+        } else {
+            stopAudio (curSong);
+            curSong = playAudio ("halloween.mp3");
+        }
+    }
+    else if (userChoice.toLocaleLowerCase() == "electronic" || userChoice.toLowerCase() == "something electronic") {
+        if (!songPlaying){
+            curSong = playAudio ("remote.mp3");
+            songPlaying = true;
+        } else {
+            stopAudio (curSong);
+            curSong = playAudio ("remote.mp3");
+        }
+    }
+}       
+
+$("#dialogue-1").click(function () {
+    console.log("mood count" + mainEntity.moodCount);
+    console.log("dia-1 is clicked");
+    fadeText("Yoroshiku! My name is " + mainEntity.name + ". I'm feeling " + mainEntity.mood[mainEntity.moodCount] + ". "); 
+    mainEntity.moodCount = mainEntity.moodCount + 1;
+    if (mainEntity.moodCount==2) { 
+        mainEntity.moodCount=0; 
+    }
+});
+
+$("#dialogue-2").click(function () {
+    console.log("food count" + mainEntity.favoriteFoodsCount);
+    console.log("dia-2 is clicked");
+    fadeText("That's not very nice! I'm " + mainEntity.name + ". My favorite food is " 
+    + mainEntity.favoriteFoods[mainEntity.favoriteFoodsCount] + ". ");
+    mainEntity.favoriteFoodsCount = mainEntity.favoriteFoodsCount + 1;
+    if (mainEntity.favoriteFoodsCount==4) { 
+        mainEntity.favoriteFoodsCount=0; 
+    }
+});
+
+$("#len").click(function() {
+    console.log("len is clicked");
+    chooseAudio ();
+});
+
+$("#hoverclock").hover(
+  function () {
+    $("#time").stop(true, true).slideDown(300);
+},
+  function () {
+    $("#time").stop(true, true).slideUp(300);
+});
+
+$("#hoverlen").hover(
+  function () {
+    $("#clickme").stop(true, true).slideDown(300);
+},
+  function () {
+    $("#clickme").stop(true, true).slideUp(300);
+});
--- garden/creature.js
+++ garden/creature.js
@@ -1,13 +1,37 @@
 // add creature to the garden
 $("#crAdd").click(function () {
 
+    // grab the value from the text input and assign it to a variable crName
     let crName=$("#crName").val();
-    console.log(crName);
-
-    if(crName.length > 2) { 
-        $("#creature-list").append("<div>"+crName+"</div>");
+    let crColor = $("#crColor").val();
+    let crEyesNum = $("#crEyesNum").val();
+    let crMood = $("#crMood").val();
+    // lets construct hmtl for eyes using FOR cycle
+    let crEyesHtml="";
+    for (let i = 0; i < crEyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>👁️</div>";
     }
 
-    $("#crName").val("")
+    // test in console
+    console.log(crName);
+    console.log(crColor);
+    console.log(crEyesNum);
+    console.log(crEyesHtml);
+    console.log(crMood);
+
+    if(crName.length > 2) { 
+        $("#creature-list").append(`
+            <div class="creature">
+                <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml}</div>
+                <div class="creature-info">${crName}</div>
+                <div class="creature-mood">(${crMood})</div>
+            </div>
+        `);
+    }
+
+    // "<div>"+crName+crColor+crEyesNum+"</div>");
+
+    $("#crName").val("") // write the value
+    //$("#crName").val(); // retrieve the value
 
 });
